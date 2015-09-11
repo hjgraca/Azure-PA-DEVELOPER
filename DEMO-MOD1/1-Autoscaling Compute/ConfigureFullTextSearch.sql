62fdc85c24c50c906b67310e0e75ccbe85407740
morcinim <morcinim@yahoo.com>
2015-09-11 14:45:47 +0200

    fixed script
    schema change to SalesLT for Adventure works LT SQL Azure DB

--- DEMO-MOD1/1-Autoscaling Compute/ConfigureFullTextSearch.sql
+++ DEMO-MOD1/1-Autoscaling Compute/ConfigureFullTextSearch.sql
@@ -1,35 +1,19 @@
- --create Full Text Catalogue
  -- http://azure.microsoft.com/blog/2015/04/30/full-text-search-is-now-available-for-preview-in-azure-sql-database/
 CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT;
 
---for local AdventureWorksLite SQL Azure database
+--for AdventureWorksLite SQL Azure database 
 CREATE UNIQUE INDEX ui_ukProductDescription ON SalesLT.ProductDescription(ProductDescriptionID); 
 CREATE FULLTEXT INDEX ON SalesLT.ProductDescription(Description) KEY INDEX ui_ukProductDescription ON ftCatalog; 
-
--- local AdventureWorks2012
-CREATE UNIQUE INDEX ui_ukProductDescription ON [Production].ProductDescription(ProductDescriptionID); 
-CREATE FULLTEXT INDEX ON [Production].ProductDescription(Description) KEY INDEX ui_ukProductDescription ON ftCatalog;
-
-ALTER FULLTEXT INDEX ON [Production].ProductDescription ENABLE; 
+ALTER FULLTEXT INDEX ON SalesLT.ProductDescription ENABLE;
 GO 
-ALTER FULLTEXT INDEX ON [Production].ProductDescription START FULL POPULATION;
-
---enable full text sereach on product Name
-USE [AdventureWorks2012]
+ALTER FULLTEXT INDEX ON SalesLT.ProductDescription START FULL POPULATION;
 GO
-CREATE FULLTEXT INDEX ON [Production].[Product] KEY INDEX [PK_Product_ProductID] ON ([ftCatalog]) WITH (CHANGE_TRACKING AUTO)
+CREATE FULLTEXT INDEX ON SalesLT.[Product] KEY INDEX [PK_Product_ProductID] ON ([ftCatalog]) WITH (CHANGE_TRACKING AUTO)
 GO
-USE [AdventureWorks2012]
+ALTER FULLTEXT INDEX ON SalesLT.[Product] ADD ([Name])
 GO
-ALTER FULLTEXT INDEX ON [Production].[Product] ADD ([Name])
-GO
-USE [AdventureWorks2012]
-GO
-ALTER FULLTEXT INDEX ON [Production].[Product] ENABLE
-GO
-ALTER FULLTEXT INDEX ON [Production].[Product] ENABLE; 
+ALTER FULLTEXT INDEX ON SalesLT.[Product] ENABLE
 GO 
-ALTER FULLTEXT INDEX ON [Production].[Product] START FULL POPULATION;
-
+ALTER FULLTEXT INDEX ON SalesLT.[Product] START FULL POPULATION;
 --Test full text search for Products
-Select Name from Production.Product Where Freetext(*,'Bike')
+Select Name from SalesLT.Product Where Freetext(*,'Bike')
